5409c96186afad18c04320fc11634ee792830876
Matt . <matt.@Matts-MacBook-Pro.local>
2026-07-07 18:41:10 +0300

    Patientor step 4 finished

---
+++ patientor/backend/src/services/patientService.ts
@@ -0,0 +1,21 @@
+import patients from "../../data/patients.ts";
+import type { NonSensitivePatient, Patient } from "../types.ts";
+
+const getPatients = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default {
+  getPatients,
+  getNonSensitivePatients,
+};
